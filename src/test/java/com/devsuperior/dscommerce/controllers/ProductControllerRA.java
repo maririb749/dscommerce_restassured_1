93794547204ee6e5c2bf4752db0a5631139fcbb8
Mariana Santos <maririb51@gmail.com>
2023-11-10 14:09:19 +0000

    findAllShouldReturnPagedProductsWithPriceGreaterThan2000

--- src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
+++ src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
@@ -76,6 +76,13 @@ public class ProductControllerRA {
 	        .body("content.price[0]", is(1250.0f))
 	        .body("content.imgUrl[0]", equalTo("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg"));
 	}
+	@Test
+	public void findAllShouldReturnPagedProductsWithPriceGreaterThan2000() {
+		given()
+			.get("/products?size=25")
+		.then()
+			.body("content.findAll { it.price > 2000 }.name", hasItems("Smart TV", "PC Gamer Weed"));
+	}
 	
 }
 
