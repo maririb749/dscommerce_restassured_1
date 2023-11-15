340d7ee812de6f3ba1bd24727b8049ef4eb60b4c
Mariana Santos <maririb51@gmail.com>
2023-11-15 10:33:55 +0000

    Inserção de produto retorna 422 e mensagens customizadas com dados inválidos quando logado como admin e campo description for inválido

--- src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
+++ src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
@@ -170,7 +170,26 @@ public class ProductControllerRA {
 	    .body("errors.message[0]", equalTo("Nome precisar ter de 3 a 80 caracteres"));
 		
 	}
-	
+	@Test
+	public void insertShouldReturnUnprocessableEntityWhenAdminLoggedandinvalidDescription() {
+		
+		postProductInstance.put("description", "ab");
+		
+		JSONObject newProduct = new JSONObject(postProductInstance);
+		
+		given()
+	    .header("Content-type", "application/json")
+	    .header("Authorization", "Bearer " + adminToken)
+	    .body(newProduct)
+	    .contentType(ContentType.JSON)
+	    .accept(ContentType.JSON)
+	.when()
+	    .post("/products")
+	.then()
+	    .statusCode(422)
+	    .body("errors.message[0]", equalTo("Descrição precisa ter no mínimo 10 caracteres"));
+
+   }
 }
 
 
