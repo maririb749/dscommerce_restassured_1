b1ed283de3c76f58147300f0b5db5ce1fa6d742a
Mariana Santos <maririb51@gmail.com>
2023-11-18 20:20:04 +0000

    Busca de pedido por id retorna pedido existente quando logado como cliente e o pedido pertence ao usuário

--- src/test/java/com/devsuperior/dscommerce/controllers/OrderControllerRA.java
+++ src/test/java/com/devsuperior/dscommerce/controllers/OrderControllerRA.java
@@ -13,8 +13,8 @@ import io.restassured.http.ContentType;
 
 public class OrderControllerRA {
 	
-	private String adminUsename, adminPassword;
-	private String adminToken;
+	private String adminUsename, adminPassword, clientUsername, clientPassword;
+	private String adminToken, clientToken;
 	
 	private Long existingOrderId;
 	
@@ -25,10 +25,12 @@ public class OrderControllerRA {
 		
 		adminUsename ="alex@gmail.com";
 		adminPassword = "123456";
+		clientUsername = "maria@gmail.com";
+		clientPassword = "123456";
 		existingOrderId = 1L;
 		
 		adminToken = TokenUtil.obtainAccessToken(adminUsename, adminPassword);
-		
+		clientToken = TokenUtil.obtainAccessToken(clientUsername, clientPassword);
 	}
 
 	@Test
@@ -49,6 +51,28 @@ public class OrderControllerRA {
 	     .body("items[0].name", equalTo("The Lord of the Rings"))
 	     .body("items[1].name", equalTo("Macbook Pro"))
 	     .body("total",is(1431.0F));
+	}
+	
+	@Test
+	public void findByIdShouldReturnOrderWhenIdExistsAndClientLogged() {
+		existingOrderId = 3L;
+	 given()
+	     .header("Content-type", "application/json")
+	     .header("Authorization", "Bearer " + clientToken)
+	     .accept(ContentType.JSON)
+	 .when()
+	     .get("/orders/{id}", existingOrderId)
+	 .then()
+	     .statusCode(200)
+	     .body("id",is (3)) 
+	     .body("moment",equalTo("2022-08-03T14:20:00Z"))
+	     .body("status",equalTo("WAITING_PAYMENT"))
+	     .body("client.name",equalTo("Maria Brown"))
+	     .body("payment.moment", equalTo(null))
+	     .body("items[0].name", equalTo("The Lord of the Rings"))
+	     .body("items[0].quantity",is(1))
+	     .body("items[0].imgUrl",equalTo("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg"))
+	     .body("total",is(90.5F));
 	}
 
 
