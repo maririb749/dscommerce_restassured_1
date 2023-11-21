2bd382d59c8df6df3612e71e960f251afe0ab2a7
Mariana Santos <maririb51@gmail.com>
2023-11-21 20:18:49 +0000

    Get deve retornar usuário quuando cliente estiver Logado.

--- src/test/java/com/devsuperior/dscommerce/controllers/UserControllerRA.java
+++ src/test/java/com/devsuperior/dscommerce/controllers/UserControllerRA.java
@@ -15,9 +15,9 @@ import io.restassured.http.ContentType;
 
 public class UserControllerRA {
 	
-	private String adminUsername, adminPassowd;
+	private String adminUsername, adminPassowd, clientUsername, clientPassword;
 	
-	private String adminToken;
+	private String adminToken, clientToken;
 	
 	
 	@BeforeEach
@@ -26,8 +26,11 @@ public class UserControllerRA {
 		
 		adminUsername = "alex@gmail.com";
 		adminPassowd = "123456";
+		clientUsername = "maria@gmail.com";
+		clientPassword = "123456";
 		
 		adminToken = TokenUtil.obtainAccessToken(adminUsername, adminPassowd);
+		clientToken = TokenUtil.obtainAccessToken(clientUsername, clientPassword);
 	}
 	
 	@Test
@@ -47,6 +50,24 @@ public class UserControllerRA {
 		.body("birthDate", equalTo("1987-12-13"))
 		.body("roles", hasItems("ROLE_CLIENT", "ROLE_ADMIN"));
     }
+	@Test
+	  public void getshouldReturnUserWhenClientLogged(){
+		given()
+		.header("Content-type", "application/json")
+		.header("Authorization", "Bearer " + clientToken)
+		.accept(ContentType.JSON)
+	.when()
+		.get("/users/me")
+	.then()
+		.statusCode(200)
+		.body("id", is(1))
+		.body("name", equalTo("Maria Brown"))
+		.body("email", equalTo("maria@gmail.com"))
+		.body("phone", equalTo("988888888"))
+		.body("birthDate", equalTo("2001-07-25"))
+		.body("roles", hasItems("ROLE_CLIENT"));
+  }
+		
 		
 }
 
