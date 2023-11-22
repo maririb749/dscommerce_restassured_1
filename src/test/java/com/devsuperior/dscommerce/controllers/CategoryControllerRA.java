fbaf7f86744c0c8bb1bbbc8dc7d0d4799f2203c4
Mariana Santos <maririb51@gmail.com>
2023-11-22 12:52:03 +0000

    findAll deve retornar lista de categorias.

---
+++ src/test/java/com/devsuperior/dscommerce/controllers/CategoryControllerRA.java
@@ -0,0 +1,32 @@
+package com.devsuperior.dscommerce.controllers;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItems;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CategoryControllerRA {
+	
+	
+	
+	@BeforeEach
+	public void SetUp() {
+		baseURI = "http://localhost:8080";
+	
+		
+	}
+	
+	@Test
+	public void findAllShouldReturnListOfCategories() {
+		given()
+			.get("/categories")
+		.then()
+			.statusCode(200)
+			.body("id", hasItems(1, 2, 3))
+			.body("name", hasItems("Livros", "Eletrônicos", "Computadores"));
+	}
+
+}
